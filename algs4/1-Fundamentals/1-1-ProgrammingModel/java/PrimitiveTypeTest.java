be6e4fc4bf8ff518814cd9bb637ed296fc898918
tzengjh <zengjinghong@hotmail.com>
2018-03-13 18:09:38 +0800

    Test of primitive data type

---
+++ algs4/1-Fundamentals/1-1-ProgrammingModel/java/PrimitiveTypeTest.java
@@ -0,0 +1,55 @@
+import java.lang.*;
+
+public class PrimitiveTypeTest { 
+	public static void main(String[] args) {  
+		//byte  
+		System.out.println("Primitive Type:byte bits:" + Byte.SIZE);  
+		System.out.println("Class:java.lang.Byte");  
+		System.out.println("Min value:Byte.MIN_VALUE=" + Byte.MIN_VALUE);  
+		System.out.println("Max value:Byte.MAX_VALUE=" + Byte.MAX_VALUE);  
+		System.out.println();  
+
+		// short  
+		System.out.println("Primitive Type:short bits:" + Short.SIZE);  
+		System.out.println("Class:java.lang.Short");  
+		System.out.println("Min value:Short.MIN_VALUE=" + Short.MIN_VALUE);  
+		System.out.println("Max value:Short.MAX_VALUE=" + Short.MAX_VALUE);  
+		System.out.println();  
+
+		// int  
+		System.out.println("Primitive Type:int bits:" + Integer.SIZE);  
+		System.out.println("Class:java.lang.Integer");  
+		System.out.println("Min value:Integer.MIN_VALUE=" + Integer.MIN_VALUE);  
+		System.out.println("Max value:Integer.MAX_VALUE=" + Integer.MAX_VALUE);  
+		System.out.println();  
+
+		// long  
+		System.out.println("Primitive Type:long bits:" + Long.SIZE);  
+		System.out.println("Class:java.lang.Long");  
+		System.out.println("Min value:Long.MIN_VALUE=" + Long.MIN_VALUE);  
+		System.out.println("Max value:Long.MAX_VALUE=" + Long.MAX_VALUE);  
+		System.out.println();  
+
+		// float  
+		System.out.println("Primitive Type:float bits:" + Float.SIZE);  
+		System.out.println("Class:java.lang.Float");  
+		System.out.println("Min value:Float.MIN_VALUE=" + Float.MIN_VALUE);  
+		System.out.println("Max value:Float.MAX_VALUE=" + Float.MAX_VALUE);  
+		System.out.println();  
+
+		// double  
+		System.out.println("Primitive Type:double bits:" + Double.SIZE);  
+		System.out.println("Class:java.lang.Double");  
+		System.out.println("Min value:Double.MIN_VALUE=" + Double.MIN_VALUE);  
+		System.out.println("Max value:Double.MAX_VALUE=" + Double.MAX_VALUE);  
+		System.out.println();  
+
+		// char  
+		System.out.println("Primitive Type:char bits:" + Character.SIZE);  
+		System.out.println("Class:java.lang.Character");  
+		System.out.println("Min value:Character.MIN_VALUE=" 
+				+ (int) Character.MIN_VALUE);  
+		System.out.println("Max value:Character.MAX_VALUE="
+				+ (int) Character.MAX_VALUE);  
+	}  
+}
