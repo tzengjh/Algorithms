4a073d4fc132b2f7fae979ec129bbf1a514fff24
tzengjh <zengjinghong@hotmail.com>
2018-03-16 11:49:44 +0800

    Add code of 1.1.19 Fibonacci

---
+++ algs4/1-Fundamentals/1-1-ProgrammingModel/1-1-19/Fibonacci.java
@@ -0,0 +1,14 @@
+import edu.princeton.cs.algs4.StdOut;
+public class Fibonacci{
+	public static long F(int N){
+		if(N == 0 || N == 1)
+			return 1;
+		else
+			return F(N - 1) + F(N - 2);
+	}
+
+	public static void main(String[] args) {
+		for(int N = 0; N < 100; N ++)
+			StdOut.println(N + " " + F(N));
+	}
+}
