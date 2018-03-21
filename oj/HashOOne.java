38b06d8f13ef483a484dd6fe9b64937c144ec60e
tzengjh <zengjinghong@hotmail.com>
2018-03-22 02:15:17 +0800

    Add ali interview O1-Hash-implementation

---
+++ oj/HashOOne.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class HashOOne{
+  private long setAllSystemTime;
+  private int globalValue;
+  private HashMap<Integer, Integer> keyValue = new HashMap<Integer, Integer>();
+  private HashMap<Integer, Long> keySetTime = new HashMap<Integer, Long>();
+
+  public HashOOne(){
+    setAllSystemTime = System.currentTimeMillis();
+  }
+
+  public void set(int key, int value){
+    keyValue.put(key, value);
+    keySetTime.put(key, System.currentTimeMillis());
+  }
+
+  public int get(int key, int defaultValue){
+    if(keySetTime.get(key) == null)
+      return defaultValue;
+    if(keySetTime.get(key) < setAllSystemTime)
+      return globalValue;
+    else
+      return keyValue.get(key);
+  }
+  
+  public void setAll(int value){
+    setAllSystemTime = System.currentTimeMillis();
+    globalValue = value;
+  }
+    
+}
