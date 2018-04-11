c3c91fc0798c4187d3ea2500b3ef6aad0bd0828c
tzengjh <zengjinghong@hotmail.com>
2018-04-11 10:51:44 +0800

    Add implementation of showing selected objects in knapsack

--- acm/package/ZeroOneKnapsack.java
+++ acm/package/ZeroOneKnapsack.java
@@ -17,15 +17,32 @@ public class ZeroOneKnapsack{
                 caps[i] = scan.nextInt();
                 values[i] = scan.nextInt();
             }
-            // int[][] maxValues = new int[N+1][C+1];
-            int[] maxValues = new int[C+1];
+            int[][] maxValues = new int[N+1][C+1];
+            // int[] maxValues = new int[C+1];
             for(int i = 1; i < N + 1; i++){
                 for(int j = C; j > 0; j--){
-                    // maxValues[i][j] = j >= caps[i-1] ? Math.max(maxValues[i-1][j], maxValues[i-1][j-caps[i-1]] + values[i-1]) : maxValues[i-1][j];
-                    maxValues[j] = j >= caps[i-1] ? Math.max(maxValues[j], maxValues[j-caps[i-1]] + values[i-1]) : maxValues[j];
+                    maxValues[i][j] = j >= caps[i-1] ? Math.max(maxValues[i-1][j], maxValues[i-1][j-caps[i-1]] + values[i-1]) : maxValues[i-1][j];
+                    // maxValues[j] = j >= caps[i-1] ? Math.max(maxValues[j], maxValues[j-caps[i-1]] + values[i-1]) : maxValues[j];
                 }
             }
-            System.out.println(maxValues[C]);
+            System.out.println(maxValues[N][C]);
+
+            // show which object is selected
+            boolean[] isSelected = new boolean[N];
+            int remains = C;
+            for(int i = N; i >= 1; i--){
+                if(maxValues[i][remains] > maxValues[i-1][remains]){
+                    isSelected[i-1] = true;
+                    remains -= caps[i-1]; // until maxValues[0][remains]
+                }
+            }
+            for(int i = 0; i < N; i++)
+                if(isSelected[i]){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            System.out.println();
         }
         scan.close();
     }
