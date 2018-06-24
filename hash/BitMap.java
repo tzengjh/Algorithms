e42cd6b7d692a3da3b4a663809a6735ff46e375c
tzengjh <zengjinghong@hotmail.com>
2018-06-25 00:32:26 +0800

    Add BitMap algorithm

---
+++ hash/BitMap.java
@@ -0,0 +1,46 @@
+
+public class BitMap{
+    // bit mapping
+    private byte[] bits;
+    private int capacity;
+
+    public BitMap(int capacity){
+        this.capacity = capacity;
+        // >> 3 equals to /8
+        bits = new byte[(capacity >> 3) + 1];
+    }
+
+    public void add(int num){
+        // index
+        int index = num >> 3;
+        // position
+        // & 0x07 equals to %8
+        int position = num & 0x07;
+        bits[index] |= 0x01 << position;
+    }
+
+    public boolean contain(int num){
+        int index = num >> 3;
+        int position = num & 0x07;
+        return (bits[index] & (0x01 << position)) != 0;
+    }
+
+    public void clear(int num){
+        int index = num >> 3;
+        int position = num & 0x07;
+        bits[index] &= ~(0x01 << position);
+    }
+
+    public static void main(String[] args) {
+        BitMap bitmap = new BitMap(100);
+        bitmap.add(7);
+        System.out.println("插入7成功");
+
+        boolean isexsit = bitmap.contain(7);
+        System.out.println("7是否存在:"+isexsit);
+
+        bitmap.clear(7);
+        isexsit = bitmap.contain(7);
+        System.out.println("7是否存在:"+isexsit);
+    }
+}
